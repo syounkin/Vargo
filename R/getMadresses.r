93cd608ed85dbf0c6e6f06d4409750ac9162ef98
vargo <jvargo@gmail.com>
2015-04-30 15:56:18 -0500

    added lines to write results to GIS file

--- R/getMadresses.r
+++ R/getMadresses.r
@@ -30,3 +30,26 @@ test <- subset(MadParcels, sn == "WASHINGTON")
 
 write.csv(MadParcels,"../../Dropbox/Vargo (1)/Maddresses.csv", row.names=F, na='', quote=FALSE) 
 
+
+########################################################
+###bring in Madison energy data and merge to GIS
+########################################################
+
+
+full <- read.csv("./results.csv", header=FALSE)
+
+#add new fields once we have full results including gas usage
+names(full) <- c("hn","sd","sn","ss","au","c", "GIS_ID","high_elec", "low_elec", "avg_elec")
+
+new <- merge(DaneParcels@data, full, by.x="vargoID", by.y="GIS_ID",  all.x=T)
+
+newproj <- "+proj=lcc +lat_1=43.0695160375 +lat_0=43.0695160375 +lon_0=-89.42222222222223 +k_0=1.0000384786 +x_0=247193.2943865888 +y_0=146591.9896367793 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=us-ft"
+ 
+DaneParcels@proj4string <- CRS(newproj)
+ 
+DaneParcels@data <- new
+writeOGR(DaneParcels, "resultsGIS", "fullResults", driver="ESRI Shapefile", overwrite_layer=T)
+ 
+
+
+
