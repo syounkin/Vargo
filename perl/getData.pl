eeeda194f57d1bde1808419fe095cfc9f122e3a6
Samuel Younkin <syounkin@stat.wisc.edu>
2015-04-30 15:48:36 -0500

    Now reads in Maddresses-with-Heat.csv and skips entries that end in a comma, i.e., have no kWh data

--- perl/getData.pl
+++ perl/getData.pl
@@ -4,7 +4,8 @@ use strict;
 use LWP::Simple;
 
 
-open IN, "</home/sgy/Dropbox/Vargo/data/Maddresses.csv" or die $!;
+# open IN, "</home/sgy/Dropbox/Vargo/data/Maddresses.csv" or die $!;
+open IN, "</home/sgy/Dropbox/Vargo/data/Maddresses-with-Heat.csv" or die $!;
 open OUT, ">/home/sgy/Dropbox/Vargo/data/results.csv" or die $!;
 
 # open IN, "</home/sgy/Dropbox/Vargo/data/Maddresses-test.csv" or die $!;
@@ -17,57 +18,63 @@ my $i = 0;
 while (my $address = <IN>) {
     if ( $i != 0 ){
 
-	print $i, " of 160852", "\t", $address;
 	chomp $address;
-	my ($number, $direction, $streetname, $streettype, $unit, $city ) = split /,/, $address;
 
-	my $url = "http://www.mge.com/customer-service/home/average-use-cost/results.htm?hn=$number&sd=$direction&sn=$streetname&ss=$streettype&au=$unit&c=$city";
 
-	my $filename = "./data/results.html";
+	if( !($address =~ m/\,$/) ){
 
-	my $rc = getstore($url, $filename);
+	    my ($number, $direction, $streetname, $streettype, $unit, $city ) = split /,/, $address;
 
-	# if (is_error($rc)) {
-	#     die "getstore of <$url> failed with $rc";
-	# }
+	    my $url = "http://www.mge.com/customer-service/home/average-use-cost/results.htm?hn=$number&sd=$direction&sn=$streetname&ss=$streettype&au=$unit&c=$city";
 
-	open HTML, "<$filename" or die $!;
+	    my $filename = "./data/results.html";
 
-	my @data;
+	    my $rc = getstore($url, $filename);
 
-	while (my $line = <HTML>) {
-	    if( $line =~ m/kWh/) {
-		if( $line =~ m/>(\d*)\&nbsp/){
-		    push(@data, $1);
+	    # if (is_error($rc)) {
+	    #     die "getstore of <$url> failed with $rc";
+	    # }
+
+	    open HTML, "<$filename" or die $!;
+
+	    my @data;
+
+	    while (my $line = <HTML>) {
+		if( $line =~ m/kWh/) {
+		    if( $line =~ m/>(\d*)\&nbsp/){
+			push(@data, $1);
+		    }
 		}
+
+		if( $line =~ m/therms/) {
+		    if( $line =~ m/>(\d*)\&nbsp/){
+			push(@data, $1);
+		    }
+		}
+
+		if( $line =~ m/days/) {
+		    if( $line =~ m/>(\d*)\ /){
+			push(@data, $1);
+		    }
+		}
+
+		if( $line =~ m/\>\$(\d*)\</) {
+		    if( $line =~ m/>\$(\d*)\</){
+			push(@data, $1);
+		    }
+		}
+
 	    }
 
-	    if( $line =~ m/therms/) {
-		if( $line =~ m/>(\d*)\&nbsp/){
-		    push(@data, $1);
-		}
-	    }
+	    print OUT $address, ",", $data[0], ",", $data[1], ",", $data[2], ",", $data[3], ",", $data[4], ",", $data[5], ",", $data[6], ",", $data[7], ",", $data[8], ",", $data[9], ",", $data[10], ",", $data[11], ",", $data[12], ",", $data[13], ",", $data[14], ",", $data[15], "\n";
 
-	    if( $line =~ m/days/) {
-		if( $line =~ m/>(\d*)\ /){
-		    push(@data, $1);
-		}
-	    }
+	    print $address, ",", $data[0], ",", $data[1], ",", $data[2], ",", $data[3], ",", $data[4], ",", $data[5], ",", $data[6], ",", $data[7], ",", $data[8], ",", $data[9], ",", $data[10], ",", $data[11], ",", $data[12], ",", $data[13], ",", $data[14], ",", $data[15], "\n";
 
-	    if( $line =~ m/\>\$(\d*)\</) {
-		if( $line =~ m/>\$(\d*)\</){
-		    push(@data, $1);
-		}
-	    }
+	    close HTML;
+
+	    $i++;
 
 	}
-
-
-	close HTML;
-
-	print OUT $address, ",", $data[0], ",", $data[1], ",", $data[2], ",", $data[3], ",", $data[4], ",", $data[5], ",", $data[6], ",", $data[7], ",", $data[8], ",", $data[9], ",", $data[10], ",", $data[11], ",", $data[12], ",", $data[13], ",", $data[14], ",", $data[15], "\n";
-
-	$i++;
 
     }else{
 
